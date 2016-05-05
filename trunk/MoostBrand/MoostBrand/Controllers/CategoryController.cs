c5df17fbdbf1b6a0383823f44f1a6e121d4ce8c2
reyrodriguez <reyrodriguez@7a2338cd-1975-4922-83aa-c9a8c3681b1e>
2016-05-05 10:28:20 +0000

    Initial upload

---
+++ trunk/MoostBrand/MoostBrand/Controllers/CategoryController.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoostBrand.DAL;
+using PagedList;
+using System.Data.Entity;
+using System.Configuration;
+
+namespace MoostBrand.Controllers
+{
+    public class CategoryController : Controller
+    {
+        MoostBrandEntities entity = new MoostBrandEntities();
+        
+        // GET: Category
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "code" : "";
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            var categories = from c in entity.Categories
+                            select c;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                categories = categories.Where(c => c.Code.Contains(searchString)
+                                       || c.Description.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "code":
+                    categories = categories.OrderByDescending(c => c.Code);
+                    break;
+                case "desc":
+                    categories = categories.OrderByDescending(c => c.Description);
+                    break;
+                default:
+                    categories = categories.OrderBy(c => c.ID);
+                    break;
+            }
+
+            int pageSize = Convert.ToInt32(ConfigurationManager.AppSettings["pageSize"]);
+            int pageNumber = (page ?? 1);
+            return View(categories.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int id)
+        {
+            var category = entity.Categories.Find(id);
+
+            return View(category);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var category = new Category();
+
+                if(collection.Count >0)
+                {
+                    category.Code = collection["Code"];
+                    category.Description = collection["Description"];
+
+                    try
+                    {
+                        entity.Categories.Add(category);
+                        entity.SaveChanges();
+                    }
+                    catch { }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var category = entity.Categories.Find(id);
+
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var category = entity.Categories.Find(id);
+
+                if (collection.Count > 0)
+                {
+                    category.Code = collection["Code"];
+                    category.Description = collection["Description"];
+
+                    try
+                    {
+                        entity.Entry(category).State = EntityState.Modified;
+                        entity.SaveChanges();
+                    }
+                    catch { }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var category = entity.Categories.Find(id);
+
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var category = entity.Categories.Find(id);
+
+                try
+                {
+                    entity.Categories.Remove(category);
+                    entity.SaveChanges();
+                }
+                catch { }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //// GET: Category/SubCategories/5
+        //public ActionResult SubCategories(int categoryid, string sortOrder, string currentFilter, string searchString, int? page)
+        //{
+        //    Session["categoryID"] = categoryid;
+        //    ViewBag.CategoryID = categoryid;
+        //    ViewBag.CurrentCategory = "REY";
+        //    ViewBag.CurrentSort = sortOrder;
+        //    ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "code" : "";
+        //    ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "desc" : "";
+
+        //    if (searchString != null)
+        //    {
+        //        page = 1;
+        //    }
+        //    else
+        //    {
+        //        searchString = currentFilter;
+        //    }
+
+        //    ViewBag.CurrentFilter = searchString;
+
+
+        //    var subcategories = from s in entity.SubCategories
+        //                     select s;
+
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        subcategories = subcategories.Where(s => (s.Code.Contains(searchString)
+        //                               || s.Description.Contains(searchString)) && s.CategoryID == categoryid);
+        //    }
+
+        //    switch (sortOrder)
+        //    {
+        //        case "code":
+        //            subcategories = subcategories.OrderByDescending(s => s.Code);
+        //            break;
+        //        case "desc":
+        //            subcategories = subcategories.OrderByDescending(s => s.Description);
+        //            break;
+        //        default:
+        //            subcategories = subcategories.OrderBy(s => s.ID);
+        //            break;
+        //    }
+        //    int pageSize = Convert.ToInt32(ConfigurationManager.AppSettings["pageSize"]);
+        //    int pageNumber = (page ?? 1);
+        //    return View(subcategories.ToPagedList(pageNumber, pageSize));
+        //}
+
+        //// GET: Category/SubCategoryDetails/5
+        //public ActionResult SubCategoryDetails(int id)
+        //{
+        //    ViewBag.CategoryID = Session["CategoryID"];
+
+        //    var subcategory = entity.SubCategories.Find(id);
+
+        //    return View(subcategory);
+        //}
+
+        //// GET: Category/SubCategoryCreate
+        //public ActionResult SubCategoryCreate()
+        //{
+        //    ViewBag.CategoryID = Session["CategoryID"];
+        //    return View();
+        //}
+
+        //// POST: Category/SubCategoryCreate
+        //[HttpPost]
+        //public ActionResult SubCategoryCreate(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+        //        var sub = new SubCategory();
+
+        //        if (collection.Count > 0)
+        //        {
+        //            sub.CategoryID = Convert.ToInt32(Session["CategoryID"]);
+        //            sub.Code = collection["Code"];
+        //            sub.Description = collection["Description"];
+        //            sub.Picture = collection["Picture"];
+        //            try
+        //            {
+        //                entity.SubCategories.Add(sub);
+        //                entity.SaveChanges();
+        //            }
+        //            catch { }
+        //        }
+
+        //        return RedirectToAction("SubCategories",new { categoryid = Convert.ToInt32(Session["CategoryID"]) });
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+        //// GET: Category/SubCategoryEdit/5
+        //public ActionResult SubCategoryEdit(int id)
+        //{
+        //    ViewBag.CategoryID = Session["CategoryID"];
+        //    var sub = entity.SubCategories.Find(id);
+
+        //    return View(sub);
+        //}
+
+        //// POST: Category/SubCategoryEdit/5
+        //[HttpPost]
+        //public ActionResult SubCategoryEdit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+        //        var sub = entity.SubCategories.Find(id);
+
+        //        if (collection.Count > 0)
+        //        {
+        //            sub.CategoryID = Convert.ToInt32(Session["CategoryID"]);
+        //            sub.Code = collection["Code"];
+        //            sub.Description = collection["Description"];
+        //            sub.Picture = collection["Picture"];
+
+        //            try
+        //            {
+        //                entity.Entry(sub).State = EntityState.Modified;
+        //                entity.SaveChanges();
+        //            }
+        //            catch { }
+        //        }
+
+        //        return RedirectToAction("SubCategories", new { categoryid = Convert.ToInt32(Session["CategoryID"]) });
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Category/SubCategoryDelete/5
+        //public ActionResult SubCategoryDelete(int id)
+        //{
+        //    ViewBag.CategoryID = Session["CategoryID"];
+
+        //    var sub = entity.SubCategories.Find(id);
+
+        //    return View(sub);
+        //}
+
+        //// POST: Category/SubCategoryDelete/5
+        //[HttpPost]
+        //public ActionResult SubCategoryDelete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+        //        var sub = entity.SubCategories.Find(id);
+
+        //        try
+        //        {
+        //            entity.SubCategories.Remove(sub);
+        //            entity.SaveChanges();
+        //        }
+        //        catch { }
+
+        //        return RedirectToAction("SubCategories", new { categoryid = Convert.ToInt32(Session["CategoryID"]) });
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+    }
+}
---
+++ trunk/MoostBrand/MoostBrand/Controllers/EmployeeController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoostBrand.DAL;
+using System.Data.Entity;
+using PagedList;
+using System.Configuration;
+
+namespace MoostBrand.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        MoostBrandEntities entity = new MoostBrandEntities();
+        
+
+        // GET: Employee
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "lastname" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            var employees = from e in entity.Employees
+                             select e;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employees = employees.Where(e => e.LastName.Contains(searchString)
+                                       || e.FirstName.Contains(searchString)
+                                       || e.Position.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "lastname":
+                    employees = employees.OrderByDescending(e => e.LastName);
+                    break;
+                default:
+                    employees = employees.OrderBy(e => e.ID);
+                    break;
+            }
+            int pageSize = Convert.ToInt32(ConfigurationManager.AppSettings["pageSize"]);
+            int pageNumber = (page ?? 1);
+            return View(employees.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = entity.Employees.Find(id);
+            return View(employee);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                Employee employee = new Employee();
+                // TODO: Add insert logic here
+
+                if(collection.Count > 0)
+                {
+                    employee.LastName = collection["LastName"];
+                    employee.FirstName = collection["FirstName"];
+                    employee.Position = collection["Position"];
+
+                    try
+                    {
+                        entity.Employees.Add(employee);
+                        entity.SaveChanges();
+                    }
+                    catch { }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var employee = entity.Employees.Find(id);
+
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var employee = entity.Employees.Find(id);
+
+                if(collection.Count > 0)
+                {
+                    employee.LastName = collection["LastName"];
+                    employee.FirstName = collection["FirstName"];
+                    employee.Position = collection["Position"];
+
+                    try
+                    {
+                        entity.Entry(employee).State = EntityState.Modified;
+                        entity.SaveChanges();
+                    }
+                    catch { }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var employee = entity.Employees.Find(id);
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var employee = entity.Employees.Find(id);
+
+                try
+                {
+                    entity.Employees.Remove(employee);
+                    entity.SaveChanges();
+                }
+                catch { }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
