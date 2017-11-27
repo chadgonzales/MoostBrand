0673dd00d3e1c046d9041eb36026f64d909e47ab
liracatabay <liracatabay@7a2338cd-1975-4922-83aa-c9a8c3681b1e>
2017-11-27 11:56:57 +0000

    add AFNI dtr

---
+++ trunk/AFNIAPI/AFNIDTR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AFNIDTR.Startup))]
+namespace AFNIDTR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
